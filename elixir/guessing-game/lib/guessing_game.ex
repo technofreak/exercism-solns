306f60b6bb85f2d3a1c14ae473fd17b224b90ba3
Parthan Ramanujam <parthan@ipro.net>
2021-12-19 16:50:04 +1100

    [elixir] Ex 8 Guessign game

---
+++ elixir/guessing-game/lib/guessing_game.ex
@@ -0,0 +1,8 @@
+defmodule GuessingGame do
+  def compare(secret_number, guess) when secret_number == guess, do: "Correct"
+  def compare(_secret_number, :no_guess), do: "Make a guess"
+  def compare(secret_number, guess) when guess+1 == secret_number or guess-1 == secret_number, do: "So close"
+  def compare(secret_number, guess) when guess > secret_number, do: "Too high"
+  def compare(secret_number, guess) when guess < secret_number, do: "Too low"
+  def compare(_secret_number), do: "Make a guess"
+end
