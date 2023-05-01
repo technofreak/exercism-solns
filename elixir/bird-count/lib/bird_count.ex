23d6d70876ce9c386caf5eea0cb7e5bcb3e39c0b
Parthan Ramanujam <parthan@ubercarshare.com>
2023-05-01 19:13:41 +1000

    [elixir] Add solution for bird-count

---
+++ elixir/bird-count/lib/bird_count.ex
@@ -0,0 +1,51 @@
+defmodule BirdCount do
+  def today([]) do
+    nil
+  end
+
+  def today(list) do
+    hd(list)
+  end
+
+  def increment_day_count([]) do
+    [1]
+  end
+
+  def increment_day_count([head | tail]) do
+    [head+1 | tail]
+  end
+
+  def has_day_without_birds?([]) do
+    false
+  end
+
+  def has_day_without_birds?([head | tail]) do
+    cond do
+      head == 0 ->
+        true
+      true ->
+        has_day_without_birds?(tail)
+    end
+  end
+
+  def total([]) do
+    0
+  end
+
+  def total([head | tail]) do
+    head + total(tail)
+  end
+
+  def busy_days([]) do
+    0
+  end
+
+  def busy_days([head | tail]) do
+    cond do
+      head > 4 ->
+        1 + busy_days(tail)
+      true ->
+        busy_days(tail)
+    end
+  end
+end
